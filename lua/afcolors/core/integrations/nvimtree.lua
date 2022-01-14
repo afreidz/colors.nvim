09a3ea8d2e368691879496889a8583c8b6606a75
afreidz <afreidenfelds@gmail.com>
2022-01-14 15:13:19 -0600

    tree updates

--- lua/afcolors/core/integrations/nvimtree.lua
+++ lua/afcolors/core/integrations/nvimtree.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.get(cp)
 	return {
 		NvimTreeFolderName = { fg = cp.blue },
-		NvimTreeFolderIcon = { fg = cp.gray0 },
+		NvimTreeFolderIcon = { fg = cp.gray1 },
 		NvimTreeNormal = { fg = cp.white, bg = cp.black1 },
 		NvimTreeOpenedFolderName = { fg = cp.blue },
 		NvimTreeEmptyFolderName = { fg = cp.blue },
@@ -18,6 +18,21 @@ function M.get(cp)
 		NvimTreeSpecialFile = { fg = cp.sky },
 		NvimTreeImageFile = { fg = cp.white },
 		NvimTreeOpenedFile = { fg = cp.pink },
+    NvimTreeLicenseIcon = { fg = cp.gray0 },
+    NvimTreeYamlIcon = { fg = cp.gray0 },
+    NvimTreeTomlIcon = { fg = cp.gray0 },
+    NvimTreeGitignoreIcon = { fg = cp.gray0 },
+    NvimTreeJsonIcon = { fg = cp.gray0 },
+    NvimTreeLuaIcon = { fg = cp.gray0 },
+    NvimTreePythonIcon = { fg = cp.gray0 },
+    NvimTreeShellIcon = { fg = cp.gray0 },
+    NvimTreeJavascriptIcon = { fg = cp.gray0 },
+    NvimTreeCIcon = { fg = cp.gray0 },
+    NvimTreeReactIcon = { fg = cp.gray0 },
+    NvimTreeHtmlIcon = { fg = cp.gray0 },
+    NvimTreeRustIcon = { fg = cp.gray0 },
+    NvimTreeVimIcon = { fg = cp.gray0 },
+    NvimTreeTypescriptIcon = { fg = cp.gray0 },
 	}
 end
 
