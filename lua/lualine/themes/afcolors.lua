8ae2cbe7c339ae609f3b2c6186853bb07e5ccb47
afreidz <afreidenfelds@gmail.com>
2022-01-14 12:18:52 -0600

    updates lualine theme

--- lua/lualine/themes/afcolors.lua
+++ lua/lualine/themes/afcolors.lua
@@ -1,36 +1,58 @@
-local cp = require("afcolors.core.color_palette")
+local cp = require("afcp.core.color_palette")
 local theme = {}
 
 theme.normal = {
-	a = { bg = cp.blue, fg = cp.black1, gui = "bold" },
-	b = { bg = cp.black4, fg = cp.blue },
-	c = { bg = cp.black1, fg = cp.white },
+  a = { bg = cp.black2, fg = cp.blue, gui = "italic" },
+  b = { bg = cp.black2, fg = cp.blue },
+  c = { bg = cp.black2, fg = cp.blue },
+  x = { bg = cp.black2, fg = cp.black4 },
+  y = { bg = cp.black2, fg = cp.black4 },
+  z = { bg = cp.black2, fg = cp.black4 },
 }
 
 theme.insert = {
-	a = { bg = cp.green, fg = cp.black2, gui = "bold" },
-	b = { bg = cp.black4, fg = cp.teal },
+  a = { bg = cp.black2, fg = cp.teal, gui = "italic" },
+  b = { bg = cp.black2, fg = cp.teal },
+  c = { bg = cp.black2, fg = cp.teal },
+  x = { bg = cp.black2, fg = cp.black4 },
+  y = { bg = cp.black2, fg = cp.black4 },
+  z = { bg = cp.black2, fg = cp.black4 },
 }
 
 theme.command = {
-	a = { bg = cp.peach, fg = cp.black2, gui = "bold" },
-	b = { bg = cp.black4, fg = cp.peach },
+  a = { bg = cp.black2, fg = cp.peach, gui = "italic" },
+  b = { bg = cp.black2, fg = cp.peach },
+  c = { bg = cp.black2, fg = cp.peach },
+  x = { bg = cp.black2, fg = cp.black4 },
+  y = { bg = cp.black2, fg = cp.black4 },
+  z = { bg = cp.black2, fg = cp.black4 },
 }
 
 theme.visual = {
-	a = { bg = cp.mauve, fg = cp.black2, gui = "bold" },
-	b = { bg = cp.black4, fg = cp.mauve },
+  a = { bg = cp.black2, fg = cp.magenta, gui = "italic" },
+  b = { bg = cp.black2, fg = cp.magenta },
+  c = { bg = cp.black2, fg = cp.magenta },
+  x = { bg = cp.black2, fg = cp.black4 },
+  y = { bg = cp.black2, fg = cp.black4 },
+  z = { bg = cp.black2, fg = cp.black4 },
 }
 
 theme.replace = {
-	a = { bg = cp.red, fg = cp.black2, gui = "bold" },
-	b = { bg = cp.black4, fg = cp.red },
+  a = { bg = cp.black2, fg = cp.red, gui = "italic" },
+  b = { bg = cp.black2, fg = cp.red },
+  c = { bg = cp.black2, fg = cp.red },
+  x = { bg = cp.black2, fg = cp.black4 },
+  y = { bg = cp.black2, fg = cp.black4 },
+  z = { bg = cp.black2, fg = cp.black4 },
 }
 
 theme.inactive = {
-	a = { bg = cp.black2, fg = cp.blue },
-	b = { bg = cp.black2, fg = cp.black4, gui = "bold" },
-	c = { bg = cp.black2, fg = cp.gray0 },
+  a = { bg = cp.black2, fg = cp.black4, gui = "italic" },
+  b = { bg = cp.black2, fg = cp.black4 },
+  c = { bg = cp.black2, fg = cp.black4 },
+  x = { bg = cp.black2, fg = cp.black4 },
+  y = { bg = cp.black2, fg = cp.black4 },
+  z = { bg = cp.black2, fg = cp.black4 },
 }
 
 return theme
