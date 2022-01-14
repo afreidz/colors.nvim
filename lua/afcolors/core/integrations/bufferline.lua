eafc86b23c438b108d86ee2b69aa5d3946dcc8db
afreidz <afreidenfelds@gmail.com>
2022-01-14 13:36:15 -0600

    bufferline updates

--- lua/afcolors/core/integrations/bufferline.lua
+++ lua/afcolors/core/integrations/bufferline.lua
@@ -1,26 +1,22 @@
 local M = {}
 
 function M.get(cp)
-
-	local inactive_bg = cp.black3
-
 	return {
-		BufferLineFill = { bg = cp.black3 },
-		BufferLineBackground = { fg = cp.white, bg = inactive_bg }, -- others
-		BufferLineBufferVisible = { fg = cp.black4, bg = inactive_bg },
+		BufferLineFill = { fg = cp.black3, bg = cp.black3 },
+		BufferLineBackground = { fg = cp.gray0, bg = cp.black3 }, -- others
+		BufferLineBufferVisible = { fg = cp.gray0, bg = cp.black2 },
 		BufferLineBufferSelected = { fg = cp.white, bg = cp.black2, style = "italic" }, -- current
-		BufferLineTab = { fg = cp.black4, bg = cp.black2 },
 		BufferLineTabSelected = { fg = cp.red, bg = cp.blue },
-		BufferLineTabClose = { fg = cp.red, bg = inactive_bg },
+		BufferLineTabClose = { fg = cp.red, bg = cp.black3 },
 		BufferLineIndicatorSelected = { fg = cp.peach, bg = cp.black2 },
 		-- separators
-		BufferLineSeparator = { fg = inactive_bg, bg = inactive_bg },
-		BufferLineSeparatorVisible = { fg = inactive_bg, bg = inactive_bg },
-		BufferLineSeparatorSelected = { fg = inactive_bg, bg = inactive_bg },
+		BufferLineSeparator = { fg = cp.black3, bg = cp.black3 },
+		BufferLineSeparatorVisible = { fg = cp.black3, bg = cp.black3 },
+		BufferLineSeparatorSelected = { fg = cp.black3, bg = cp.black3 },
 		-- close buttons
-		BufferLineCloseButton = { fg = cp.black4, bg = inactive_bg },
-		BufferLineCloseButtonVisible = { fg = cp.black4, bg = inactive_bg },
-		BufferLineCloseButtonSelected = { fg = cp.red, bg = cp.black2 },
+		BufferLineCloseButton = { fg = cp.black4, bg = cp.black3 },
+		BufferLineCloseButtonVisible = { fg = cp.black4, bg = cp.black2 },
+		BufferLineCloseButtonSelected = { fg = cp.gray0, bg = cp.black2 },
 	}
 end
 
