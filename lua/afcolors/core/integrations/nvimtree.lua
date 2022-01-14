b3c325b54273b69f21a9080127e561878bf8a928
afreidz <afreidenfelds@gmail.com>
2022-01-14 13:47:26 -0600

    tree updates

--- lua/afcolors/core/integrations/nvimtree.lua
+++ lua/afcolors/core/integrations/nvimtree.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.get(cp)
 	return {
 		NvimTreeFolderName = { fg = cp.blue },
-		NvimTreeFolderIcon = { fg = cp.black4 },
+		NvimTreeFolderIcon = { bg = cp.black4 },
 		NvimTreeNormal = { fg = cp.white, bg = cp.black1 },
 		NvimTreeOpenedFolderName = { fg = cp.blue },
 		NvimTreeEmptyFolderName = { fg = cp.blue },
