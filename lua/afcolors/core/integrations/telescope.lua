ac31cd9d8d83941eda2945cc0de33ee9e6ac2c1d
afreidz <afreidenfelds@gmail.com>
2022-01-21 10:04:38 -0600

    telescope updates

--- lua/afcolors/core/integrations/telescope.lua
+++ lua/afcolors/core/integrations/telescope.lua
@@ -2,10 +2,10 @@ local M = {}
 
 function M.get(cp)
 	return {
-    TelescopeNormal = { bg = cp.black2 },
+    TelescopeNormal = { bg = cp.black3 },
     TelescopePromptNormal = { bg = cp.black3 },
 		TelescopePreviewNormal = { bg = cp.black1 },
-    TelescopeBorder = { bg = cp.black2, fg = cp.black2 },
+    TelescopeBorder = { bg = cp.black3, fg = cp.black3 },
     TelescopePromptBorder = { bg = cp.black3, fg = cp.black3 },
     TelescopePreviewBorder = { bg = cp.black1, fg = cp.black1 },
     TelescopePromptTitle = { bg = cp.black3, fg = cp.red, style = 'italic' },
