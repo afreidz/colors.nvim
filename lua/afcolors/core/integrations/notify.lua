0af40f9eb643b1d519596fbcc040b01bea069470
afreidz <afreidenfelds@gmail.com>
2022-01-24 10:01:54 -0600

    updates to notify

--- lua/afcolors/core/integrations/notify.lua
+++ lua/afcolors/core/integrations/notify.lua
@@ -5,29 +5,29 @@ function M.get(cp)
   local bg = cp.black4
 
 	return {
+    NotifyBody = { bg = bg, fg = cp.red },
 		NotifyERRORBorder = { bg = bg, fg = bg },
 		NotifyERRORIcon = { bg = bg, fg = cp.red },
-    NotifyERRORBody = { bg = bg, fg = cp.red },
 		NotifyERRORTitle = { bg = bg, fg = cp.red, style = "italic" },
 
     NotifyWARNBorder = { bg = bg, fg = bg },
+    NotifyBody = { bg = bg, fg = cp.yellow },
 		NotifyWARNIcon = { bg = bg, fg = cp.yellow },
-    NotifyWARNBody = { bg = bg, fg = cp.yellow },
 		NotifyWARNTitle = { bg = bg, fg = cp.yellow, style = "italic" },
 
+    NotifyBody = { bg = bg, fg = cp.blue },
     NotifyINFOBorder = { bg = bg, fg = bg },
 		NotifyINFOIcon = { bg = bg, fg = cp.blue },
-    NotifyINFOBody = { bg = bg, fg = cp.blue },
 		NotifyINFOTitle = { bg = bg, fg = cp.blue, style = "italic" },
   
+    NotifyBody = { bg = bg, fg = cp.peach },
     NotifyDEBUGBorder = { bg = bg, fg = bg },
 		NotifyDEBUGIcon = { bg = bg, fg = cp.peach },
-    NotifyDEBUGBody = { bg = bg, fg = cp.peach },
 		NotifyDEBUGTitle = { bg = bg, fg = cp.peach, style = "italic" },
 
     NotifyTRACEBorder = { bg = bg, fg = bg },
+    NotifyBody = { bg = bg, fg = cp.rosewater },
 		NotifyTRACEIcon = { bg = bg, fg = cp.rosewater },
-    NotifyTRACEBody = { bg = bg, fg = cp.rosewater },
 		NotifyTRACETitle = { bg = bg, fg = cp.rosewater, style = "italic" },
   }
 end
