473d8b362c64a9c2a5958a78136925054b1a1a5d
afreidz <afreidenfelds@gmail.com>
2022-01-24 09:52:12 -0600

    updates to notify

--- lua/afcolors/core/integrations/notify.lua
+++ lua/afcolors/core/integrations/notify.lua
@@ -1,23 +1,35 @@
 local M = {}
 
 function M.get(cp)
+  
+  local bg = cp.black4
+
 	return {
-		NotifyERRORBorder = { fg = cp.red },
-		NotifyERRORIcon = { fg = cp.red },
-		NotifyERRORTitle = { fg = cp.red, style = "italic" },
-		NotifyWARNBorder = { fg = cp.yellow },
-		NotifyWARNIcon = { fg = cp.yellow },
-		NotifyWARNTitle = { fg = cp.yellow, style = "italic" },
-		NotifyINFOBorder = { fg = cp.blue },
-		NotifyINFOIcon = { fg = cp.blue },
-		NotifyINFOTitle = { fg = cp.blue, style = "italic" },
-		NotifyDEBUGBorder = { fg = cp.peach },
-		NotifyDEBUGIcon = { fg = cp.peach },
-		NotifyDEBUGTitle = { fg = cp.peach, style = "italic" },
-		NotifyTRACEBorder = { fg = cp.rosewater },
-		NotifyTRACEIcon = { fg = cp.rosewater },
-		NotifyTRACETitle = { fg = cp.rosewater, style = "italic" },
-	}
+		NotifyERRORBorder = { bg = bg, fg = bg },
+		NotifyERRORIcon = { bg = bg, fg = cp.red },
+    NotifyERRORBody = { bg = bg, fg = cp.red },
+		NotifyERRORTitle = { bg = bg, fg = cp.red, style = "italic" },
+
+    NotifyWARNBorder = { bg = bg, fg = bg },
+		NotifyWARNIcon = { bg = bg, fg = cp.yellow },
+    NotifyWARNBody = { bg = bg, fg = cp.yellow },
+		NotifyWARNTitle = { bg = bg, fg = cp.yellow, style = "italic" },
+
+    NotifyINFOBorder = { bg = bg, fg = bg },
+		NotifyINFOIcon = { bg = bg, fg = cp.blue },
+    NotifyINFOBody = { bg = bg, fg = cp.blue },
+		NotifyINFOTitle = { bg = bg, fg = cp.blue, style = "italic" },
+  
+    NotifyDEBUGBorder = { bg = bg, fg = bg },
+		NotifyDEBUGIcon = { bg = bg, fg = cp.peach },
+    NotifyDEBUGBody = { bg = bg, fg = cp.peach },
+		NotifyDEBUGTitle = { bg = bg, fg = cp.peach, style = "italic" },
+
+    NotifyTRACEBorder = { bg = bg, fg = bg },
+		NotifyTRACEIcon = { bg = bg, fg = cp.rosewater },
+    NotifyTRACEBody = { bg = bg, fg = cp.rosewater },
+		NotifyTRACETitle = { bg = bg, fg = cp.rosewater, style = "italic" },
+  }
 end
 
 return M
